900e2e8e58acc2ae3d3cea876b80eab3b08393d7
java0594 <juanvasquez0594@gmail.com>
2025-12-18 15:39:03 -0500

    Ajuste de debilidades por tipo

--- shared.js
+++ shared.js
@@ -366,10 +366,19 @@ async function mostrarPokemonInterno(entrada) {
     </div>
   `
 
-  document.getElementById('btn-fav').onclick = () => {
-    toggleFavorito(pokemon)
-   // mostrarPokemonInterno(pokemon.name)
-  }
+ document.getElementById("btn-fav").onclick = () => {
+    toggleFavorito(pokemon);
+    actFav();
+  };
+}
+
+function actFav() {
+  const x = document.getElementById("btn-fav");
+  if (x.classList.contains("active")) {
+    x.classList.remove("active");
+  } else {
+    x.classList.add("active");
+  }
 }
 
 // -----------------------------------------------------
@@ -737,9 +746,43 @@ function vsBatallar() {
   // 2. TABLA DE TIPOS (simplificada)
   // -------------------------
   const TYPE_CHART = {
-    fire: { grass: 2, water: 0.5, fire: 0.5 },
-    grass: { water: 2, fire: 0.5, grass: 0.5 },
-    water: { fire: 2, grass: 0.5, water: 0.5 },
+      normal:  { rock: 0.5, ghost: 0, steel: 0.5 },
+
+  fire:    { grass: 2, ice: 2, bug: 2, steel: 2, fire: 0.5, water: 0.5, rock: 0.5, dragon: 0.5 },
+
+  water:   { fire: 2, ground: 2, rock: 2, water: 0.5, grass: 0.5, dragon: 0.5 },
+
+  electric:{ water: 2, flying: 2, electric: 0.5, grass: 0.5, dragon: 0.5, ground: 0 },
+
+  grass:   { water: 2, ground: 2, rock: 2, fire: 0.5, grass: 0.5, poison: 0.5, flying: 0.5, bug: 0.5, dragon: 0.5, steel: 0.5 },
+
+  ice:     { grass: 2, ground: 2, flying: 2, dragon: 2, fire: 0.5, water: 0.5, ice: 0.5, steel: 0.5 },
+
+  fighting:{ normal: 2, ice: 2, rock: 2, dark: 2, steel: 2, poison: 0.5, flying: 0.5, psychic: 0.5, bug: 0.5, fairy: 0.5, ghost: 0 },
+
+  poison:  { grass: 2, fairy: 2, poison: 0.5, ground: 0.5, rock: 0.5, ghost: 0.5, steel: 0 },
+
+  ground:  { fire: 2, electric: 2, poison: 2, rock: 2, steel: 2, grass: 0.5, bug: 0.5, flying: 0 },
+
+  flying:  { grass: 2, fighting: 2, bug: 2, electric: 0.5, rock: 0.5, steel: 0.5 },
+
+  psychic: { fighting: 2, poison: 2, psychic: 0.5, steel: 0.5, dark: 0 },
+
+  bug:     { grass: 2, psychic: 2, dark: 2, fire: 0.5, fighting: 0.5, poison: 0.5, flying: 0.5, ghost: 0.5, steel: 0.5, fairy: 0.5 },
+
+  rock:    { fire: 2, ice: 2, flying: 2, bug: 2, fighting: 0.5, ground: 0.5, steel: 0.5 },
+
+  ghost:   { psychic: 2, ghost: 2, dark: 0.5, normal: 0 },
+
+  dragon:  { dragon: 2, steel: 0.5, fairy: 0 },
+
+  dark:    { psychic: 2, ghost: 2, fighting: 0.5, dark: 0.5, fairy: 0.5 },
+
+  steel:   { ice: 2, rock: 2, fairy: 2, fire: 0.5, water: 0.5, electric: 0.5, steel: 0.5 },
+
+  fairy:   { fighting: 2, dragon: 2, dark: 2, fire: 0.5, poison: 0.5, steel: 0.5 }
+    
+    
   }
 
   function getMultiplier(attackerTypes, defenderTypes) {
